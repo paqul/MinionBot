6d9e32deb22c9b34015b74161645589267c14d1d
paqul <paqul.timeless@gmail.com>
2023-04-24 13:30:52 +0200

    add new channel #testy

--- bot_config.py
+++ bot_config.py
@@ -11,7 +11,7 @@ import asyncio
 import sys
 
 from discord.ext import commands
-channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy", "gra", "dungeonsanddragons", "neuroshima", "zew", "rzuty-w-trakcie-sesji", "DD", "ZEW"]
+channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy", "gra", "dungeonsanddragons", "neuroshima", "zew", "rzuty-w-trakcie-sesji", "testy", "DD", "ZEW"]
 channels_on_test = ["sala_spotkań", "dział_techcznicny", "general"]
 
 # Discord music feature of bot Initialization
