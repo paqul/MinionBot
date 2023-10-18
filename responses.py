aa50421020c4fc7ab454cf487fc83a213bb7c418
mfriik <78402873+mfriik@users.noreply.github.com>
2023-10-18 23:41:36 +0200

    changed invocation method due to minion id changing

--- responses.py
+++ responses.py
@@ -81,7 +81,7 @@ def handle_response(msg, author, author_id) -> str:
         return "Dzień Dobry, Chciałym poinformować, że jesteś osobą która nie jest milie widziana na tym kanale, wobec tego administracja kanału uprasza o nie podejmowanie kolejnych prób dołączenia. Z góry dziękuję, życzę miłego dnia."
     elif msg == "welcome":
         return f"{author} witamy na serwerze 'Władcy Kości'!"
-    elif msg == "<@1055576642254286938> statystyki dnd":
+    elif msg == "statystyki_dnd":
         return roll_dnd_stat_block(author)
     else:
         pass #do rest
