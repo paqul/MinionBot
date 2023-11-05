f3a0144ae341ee695b1ad5b8edb71cbad1335d94
paqul <paqul.timeless@gmail.com>
2023-11-05 11:14:32 +0000

    Add channel to channel_on

--- bot_config.py
+++ bot_config.py
@@ -15,7 +15,7 @@ from discord.ext import commands
 channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy",
                "gra", "gra-u-szadka", "dungeonsanddragons", "neuroshima",
                "zew", "rzuty-w-trakcie-sesji", "testy", "DD", "ZEW", "WARHAMMER", "GRA",
-               "jednostrzały", "DD 5e", "CYBERPUNKRED"]
+               "jednostrzały", "DD 5e", "CYBERPUNKRED", "Sesja publiczna"]
 channels_on_test = ["sala_spotkań", "dział_techcznicny", "general"]
 
 # Discord music feature of bot Initialization
