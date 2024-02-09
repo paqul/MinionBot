30420adcf93c7f56c630fcc748eddb61b4e986e8
mfriik <78402873+mfriik@users.noreply.github.com>
2024-02-09 11:43:29 +0100

    Update responses.py

--- responses.py
+++ responses.py
@@ -7,7 +7,7 @@ sorry_response = (
     "Po wiecej informacji i pomoc napisz komendę *help*"
 )    
 help_response = (
-    "Aby uzyskać wynik rzutu kością wpisz komendę *<ilość_kości>***k***<ilość_ściań_kości>* (np. 1k100, 3k20, 2k10, itp.)\n"
+    "Aby uzyskać wynik rzutu kością wpisz komendę *<ilość_kości>*"+"**k**"+"*<ilość_ściań_kości>* (np. 1k100, 3k20, 2k10, itp.)\n"
     "Obecnie wspierane kości " + dices_imported + ".\n"
     "Dostępne Funkcje dodatkowe:\n"
     "- Rzut Przewaga/Utrudnienie D&D 5e: 1k20a lub 1k20d\n"
