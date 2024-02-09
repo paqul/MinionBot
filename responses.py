4bf3f5bdd03d9668faebe0f72883cef679d8e398
mfriik <78402873+mfriik@users.noreply.github.com>
2024-02-09 09:28:16 +0100

    formatting fix for roll response msg

--- responses.py
+++ responses.py
@@ -38,7 +38,7 @@ def handle_response(msg, author, author_id) -> str:
         double = False if not double_bonus_or_penalty else True
         roll_response = roll_bonus_penalty(author, amount_of_rolls, dice, bonus_or_penalty, double) 
     if roll_response and len(roll_response) > 1999:
-        roll_response = roll_response[:1899] + "**Rzut przekroczył dozwolony limit znaków w wiadomości discord, spróbuj zmniejszyć ilość rzutów**"  # Truncate and add explanation
+        roll_response = roll_response[:1897] +"..." + "**" + "Rzut przekroczył dozwolony limit znaków w wiadomości discord, spróbuj zmniejszyć ilość rzutów" + "**"  # Truncate and add explanation
         return roll_response 
     elif msg == "help":
         return "Aby uzyskać wynik rzutu kością rzuć np. 1k100, 3k20, 2k10, itp. (zasada poprawnych rzutów: <ilość_kości>k<ilość_ściań_kości>) Obecnie wspierane kości [2, 3, 4, 6, 8, 10, 12, 16, 20, 100, 1000]"
