b5518d01860a1a78dcdb4b498f33c14d6399a6b3
mfriik <78402873+mfriik@users.noreply.github.com>
2024-08-07 15:46:36 +0200

    Corrected sequence of pattern recognition
    Moved Matches to Elifs to mitigate redundant matching and fix issue when some rolls would not be matched correctly

--- responses.py
+++ responses.py
@@ -29,7 +29,8 @@ character_limit_response = (
 def handle_response(msg, author, author_id) -> str:
     msg = msg.lower()
     roll_response = sorry_response
-    #Regular Roll Patttern
+
+    #Regular Roll Pattern
     regular_roll_pattern = r'(\d+)[kd](\d+)$'
     regular_roll_pattern_match = re.match(regular_roll_pattern, msg)
     if regular_roll_pattern_match:
@@ -39,36 +40,36 @@ def handle_response(msg, author, author_id) -> str:
             roll_response = morkborg_roll(author, amount_of_rolls, dice)
         else:
             roll_response = roll(author, amount_of_rolls, dice)
+
     #Roll with Modifier
-    modifier_roll_pattern = r'(\d+)[kd](\d+)([\+\-\*\/])(.*)'
-    modifier_roll_pattern_match = re.match(modifier_roll_pattern, msg)
-    if modifier_roll_pattern_match:
+    elif re.match(r'(\d+)[kd](\d+)([\+\-\*])(.*)', msg):
+        modifier_roll_pattern_match = re.match(r'(\d+)[kd](\d+)([\+\-\*])(.*)', msg)
         amount_of_rolls = int(modifier_roll_pattern_match.group(1))
         dice = int(modifier_roll_pattern_match.group(2))
         operator = modifier_roll_pattern_match.group(3)
         equation = modifier_roll_pattern_match.group(4)
         roll_response = roll_with_modifier(author, amount_of_rolls, dice, operator, equation)
+
     #Advantage/Disadvantage Roll Matching
-    dnd5_ad_roll_pattern = r'(\d+)[kd](20)(?:([ad])([\+\-\*\/])(.*))?'
-    dnd5_ad_roll_pattern_match = re.match(dnd5_ad_roll_pattern, msg)
-    if dnd5_ad_roll_pattern_match:
+    elif re.match(r'(\d+)[kd](20)(?:([ad])([\+\-\*\/])(.*))?', msg):
+        dnd5_ad_roll_pattern_match = re.match(r'(\d+)[kd](20)(?:([ad])([\+\-\*\/])(.*))?', msg)
         amount_of_rolls = int(dnd5_ad_roll_pattern_match.group(1))
         dice = int(dnd5_ad_roll_pattern_match.group(2))
         bonus = dnd5_ad_roll_pattern_match.group(3)
         operator = dnd5_ad_roll_pattern_match.group(4) if dnd5_ad_roll_pattern_match.group(4) else ''
         equation = dnd5_ad_roll_pattern_match.group(5) if dnd5_ad_roll_pattern_match.group(5) else ''
-        roll_response = dis_advantage_dnd_roll(author, amount_of_rolls, dice, bonus,operator, equation)
+        roll_response = dis_advantage_dnd_roll(author, amount_of_rolls, dice, bonus, operator, equation)
+
     #MorkBorg k66 
-    morkborg_roll_pattern = r'(\d+)[kd](66)$'
-    morkborg_roll_pattern_match = re.match(morkborg_roll_pattern, msg)
-    if morkborg_roll_pattern_match:
+    elif re.match(r'(\d+)[kd](66)$', msg):
+        morkborg_roll_pattern_match = re.match(r'(\d+)[kd](66)$', msg)
         amount_of_rolls = int(morkborg_roll_pattern_match.group(1))
         dice = int(morkborg_roll_pattern_match.group(2))
         roll_response = morkborg_roll(author, amount_of_rolls, dice)             
+
     # Call of Cthulu BonusPenalty Dice
-    callofcthulu_kp_roll_pattern = r'(\d+)([kd])(\d+)([kp])([kp]?)$'
-    callofcthulu_kp_roll_pattern_match = re.match(callofcthulu_kp_roll_pattern, msg)
-    if callofcthulu_kp_roll_pattern_match:
+    elif re.match(r'(\d+)([kd])(\d+)([kp])([kp]?)$', msg):
+        callofcthulu_kp_roll_pattern_match = re.match(r'(\d+)([kd])(\d+)([kp])([kp]?)$', msg)
         amount_of_rolls = int(callofcthulu_kp_roll_pattern_match.group(1))
         dice = int(callofcthulu_kp_roll_pattern_match.group(3))
         bonus_or_penalty = callofcthulu_kp_roll_pattern_match.group(4)
@@ -80,10 +81,12 @@ def handle_response(msg, author, author_id) -> str:
             roll_response = bonus_penalty_callofcthulu_roll(author, amount_of_rolls, dice, bonus_or_penalty, twice)
         else: 
             roll_response = None 
+
     # Truncate if response exceeds character limit
     if roll_response and len(roll_response) > 1999:
         roll_response = roll_response[:1999-(len(character_limit_response))] + " " + character_limit_response 
         return roll_response 
+
     # Return response handling for all above
     if roll_response is not None and roll_response != sorry_response:
         return roll_response
