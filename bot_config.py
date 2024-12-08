0ed86631dfbde4a330500aefda257ad83ddb35bb
paqul <paqul.timeless@gmail.com>
2024-12-08 23:23:12 +0100

    Update and Test

--- bot_config.py
+++ bot_config.py
@@ -131,9 +131,12 @@ async def send_msg(msg, user_msg, bot_self_mention_string, private):
 async def get_role(member):
     try:
         role_name = roles.handle_roles(member)
+        print(role_name)
         role1 = discord.utils.get(member.guild.roles, name=role_name)
+        print(type(role1))
+        print(role1)
         # role2 = discord.utils.get(member.guild.roles, name=role_name[1])
-        await member.add_roles([role1])
+        await member.add_roles(*role1)
     except Exception as E:
         print(E)
 
