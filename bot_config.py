179f22fe5167a78534f4cc12aca37556082250f4
mfriik <78402873+mfriik@users.noreply.github.com>
2025-02-07 18:16:26 +0100

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -11,7 +11,7 @@ import asyncio
 import sys
 
 from discord.ext import commands
-channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy", "gra", "gra-u-szadka", "dungeonsanddragons", "neuroshima", "zew", "rzuty-w-trakcie-sesji", "testy", "DD", "ZEW", "WARHAMMER", "GRA", "DCC", "OSE", "jednostrzały", "DD 5e", "CYBERPUNKRED", "Sesja publiczna", "SESJA PUBLICZNA", "generau-czat"]
+channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy", "gra", "gra-u-szadka", "dungeonsanddragons", "neuroshima", "zew", "rzuty-w-trakcie-sesji", "testy", "DD", "ZEW", "WARHAMMER", "GRA", "DCC", "OSE", "jednostrzały", "DD 5e", "CYBERPUNKRED", "Sesja publiczna", "SESJA PUBLICZNA", "generau-czat", "Mothership", "Traveller", "DELTA GREEN"]
 channels_on_test = ["sala_spotkań", "dział_techcznicny", "general"]
 
 # Discord music feature of bot Initialization
