85e9201e8e6e9ffe1e9f316dda020e777ade6945
mfriik <78402873+mfriik@users.noreply.github.com>
2025-02-20 22:05:39 +0100

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -14,10 +14,7 @@ import time
 # import audio
 
 key = token
-channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy",
-               "gra", "gra-u-szadka", "dungeonsanddragons", "neuroshima",
-               "zew", "rzuty-w-trakcie-sesji", "testy", "DD", "ZEW", "WARHAMMER", "GRA",
-               "jednostrzały", "DD 5e", "CYBERPUNKRED", "Sesja publiczna", "SESJA PUBLICZNA", "generau-czat"]
+channels_on = ["sala_spotkań", "dział_techcznicny", "warhammer", "darkheresy", "gra", "gra-u-szadka", "dungeonsanddragons", "neuroshima", "zew", "rzuty-w-trakcie-sesji", "testy", "DD", "ZEW", "WARHAMMER", "GRA", "DCC", "OSE", "jednostrzały", "DD 5e", "CYBERPUNKRED", "Sesja publiczna", "SESJA PUBLICZNA", "generau-czat"]
 channels_on_test = ["sala_spotkań", "dział_techcznicny", "general"]
 bot_self_mention_string = ""
 auto_test_task = None  # Define the auto_test_task variable globally
